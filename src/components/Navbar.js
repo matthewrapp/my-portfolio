3f17baa8dda605164eafdfd87e176add6ce33b39
matthewrapp <mattrapp25@gmail.com>
2021-07-28 16:20:57 -0600

    responsive changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,17 @@ export default function Navbar() {
   return (
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a href="#about" className="title-font font-medium text-white mb-4 md:mb-0 md:ml-3 text-xl">
+        <a href="#about" className="title-font font-medium text-white mb-4 md:mb-0 md:ml-3 text-xl md:block hidden">
           Matthew Rapp
         </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-white">
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base md:justify-center justify-around md:w-auto w-80">
+          <a href="#projects" className="md:mr-5 hover:text-white">
             Past Work
           </a>
-          <a href="#skills" className="mr-5 hover:text-white">
+          <a href="#skills" className="md:mr-5 hover:text-white">
             Skills
           </a>
-          <a href="https://drive.google.com/file/d/1M1Ko4iCIiHfvIzqY1Y1K-mQBIUHQhg2_/view?usp=sharing" target='_blank' rel="noopener noreferrer" className="mr-5 hover:text-white">
+          <a href="https://drive.google.com/file/d/1M1Ko4iCIiHfvIzqY1Y1K-mQBIUHQhg2_/view?usp=sharing" target='_blank' rel="noopener noreferrer" className="md:mr-5 hover:text-white">
             Resumé
           </a>
         </nav>
